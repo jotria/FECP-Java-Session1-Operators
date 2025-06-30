4a4d76add56beb520914be6c3b774d2de2d6f67d
Joana Tria <joana.tria@GXCHLAP00383.local>
2025-06-30 14:46:33 +0800

    Java Lab1 Submission

---
+++ Readme.md
@@ -0,0 +1,8 @@
+## Joana S. Tria
+# Lab 1: Celsius to Fahrenheit Converter
+
+Sample Output 1:
+![img.png](img.png)
+
+Sample Output 2:
+![img_1.png](img_1.png)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,16 @@ package org.example;
 import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static float convertToFahrenheit(float celsius){
+        return (celsius*((float)9 /5))+32;
+    }
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter temperature in Celsius: ");
+        float celsius = scanner.nextFloat();
+
+        float fahrenheitTemp = convertToFahrenheit(celsius);
+        System.out.printf("Temperature in Fahrenheit: %.2f%n", fahrenheitTemp);
     }
 }
